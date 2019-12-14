7af12efe5ca1b2174f981a7faa400dcf42947942
bdbai <bdbaiapp@163.com>
2019-12-14 17:11:12 +0800

    Fix randomDouble > 1

--- src/method.cc
+++ src/method.cc
@@ -218,7 +218,7 @@ unordered_multimap<string, method> methodMap = {
               -> variant<int32_t, double, string> {
               static std::random_device rd;
               static std::mt19937 gen(rd());
-              static std::uniform_real_distribution<> dis(1.0, 2.0);
+              static std::uniform_real_distribution<> dis(0.0, 1.0);
               return dis(gen);
           })}},
     {"randomInt",
